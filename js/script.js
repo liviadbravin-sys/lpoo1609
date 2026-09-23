75a789bad265cf79d5d7c352702a8f80bcc32739
liviadbravin-sys <livia.dbravin@aluno.edu.es.gov.br>
2026-09-23 12:01:07 -0300

    lpoo

--- js/script.js
+++ js/script.js
@@ -16,11 +16,17 @@ const loop = setInterval(() => {
 
     console.log(marioPosition);
     
-    if (pipePosition <= 120 && marioPosition < 80){
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80){
 
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
         
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './images/game-over.png';
+        mario.style.width = '75px'
+        mario.style.marginLeft = '50px'
     }
 
 }, 10);
